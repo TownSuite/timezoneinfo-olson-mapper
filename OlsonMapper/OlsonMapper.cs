72906de965b511fddd3dd8c77d6648faf425d209
Lance McNearney <lance@mcnearney.net>
2013-08-13 11:05:19 -0700

    Initial commit

---
+++ OlsonMapper/OlsonMapper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace McNearney.TimeZones
+{
+    public class OlsonMapper
+    {
+        /// <summary>
+        /// Holds the Windows TimeZoneId -> OlsonId mappings
+        /// </summary>
+        private readonly Dictionary<string, string> _mapping;
+
+        /// <summary>
+        /// Initializes the mapping based on the incoming file path
+        /// </summary>
+        /// <param name="path">The path to the XML file containing the mapping data</param>
+        public OlsonMapper(string path)
+        {
+            var xml = XElement.Load(path);
+            var values = from m in xml.XPathSelectElements("*/mapTimezones/mapZone")
+                         where m.Attribute("territory").Value == "001"
+                         select new
+                         {
+                            TimeZoneId = m.Attribute("other").Value,
+                            OlsonId = m.Attribute("type").Value
+                         };
+
+            _mapping = values.ToDictionary(v => v.TimeZoneId, v => v.OlsonId);
+        }
+
+        /// <summary>
+        /// Returns the OlsonId value for the specified Windows TimeZoneId
+        /// </summary>
+        /// <param name="timeZoneId">The Windows TimeZoneId primary key</param>
+        /// <returns></returns>
+        public string Find(string timeZoneId)
+        {
+            if(!_mapping.ContainsKey(timeZoneId))
+            {
+                throw new ArgumentException("Invalid TimeZoneId Specified", "timeZoneId");
+            }
+
+            return _mapping[timeZoneId];
+        }
+    }
+}
---
+++ OlsonMapper/OlsonMapperTests.cs
@@ -0,0 +1,33 @@
+﻿using MbUnit.Framework;
+
+namespace McNearney.TimeZones
+{
+    [TestFixture]
+    class OlsonMapperTests
+    {
+        private OlsonMapper _mapper;
+
+        [FixtureSetUp]
+        public void FixtureSetUp()
+        {
+            const string path = @"..\..\Data\WindowsZones.xml";
+            _mapper = new OlsonMapper(path);
+        }
+
+        [Test]
+        [Row("Pacific Standard Time", "America/Los_Angeles")]
+        [Row("SE Asia Standard Time", "Asia/Bangkok")]
+        public void Find_Returns_Correct_Result(string timeZoneId, string olsonId)
+        {
+            var result = _mapper.Find(timeZoneId);
+            Assert.AreEqual(olsonId, result);
+        }
+
+        [Test]
+        [ExpectedArgumentException]
+        public void Find_Throws_Exception_On_Invalid_TimeZoneId()
+        {
+            _mapper.Find("Unknown TimeZoneId");
+        }
+    }
+}
