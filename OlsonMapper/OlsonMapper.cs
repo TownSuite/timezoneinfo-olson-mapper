3c0b15f30a531c556e9393e3dbdd0614a3130b91
Peter Gill <peter.gill@townsuite.com>
2016-02-05 16:25:08 -0330

    Load from path or XElement

--- OlsonMapper/OlsonMapper.cs
+++ OlsonMapper/OlsonMapper.cs
@@ -17,15 +17,18 @@ namespace McNearney.TimeZones
         /// Initializes the mapping based on the incoming file path
         /// </summary>
         /// <param name="path">The path to the XML file containing the mapping data</param>
-        public OlsonMapper(string path)
+        public OlsonMapper(string path) : this(XElement.Load(path))
         {
-            var xml = XElement.Load(path);
+        }
+
+        public OlsonMapper(XElement xml)
+        {
             var values = from m in xml.XPathSelectElements("*/mapTimezones/mapZone")
                          where m.Attribute("territory").Value == "001"
                          select new
                          {
-                            TimeZoneId = m.Attribute("other").Value,
-                            OlsonId = m.Attribute("type").Value
+                             TimeZoneId = m.Attribute("other").Value,
+                             OlsonId = m.Attribute("type").Value
                          };
 
             _mapping = values.ToDictionary(v => v.TimeZoneId, v => v.OlsonId);
